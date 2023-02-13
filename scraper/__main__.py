b082978b4a184708c520eb4ed201be7b5d07ee40
Noah Bresler <noahbresler2002@gmail.com>
2023-02-13 16:00:25 -0500

    Set postgres application name

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -96,6 +96,7 @@ if __name__ == "__main__":
             password=os.getenv("DB_PASS"),
             host=os.getenv("DB_HOST"),
             port=os.getenv("DB_PORT"),
+            application_name="Aggregator",
         )
 
         scheduler.start()
