23fd93fa531422efc23449f43c5bd9a5a73bf276
Noah Bresler <noahbresler2002@gmail.com>
2023-02-13 00:06:34 -0500

    Parse get_vehicles and insert into table

--- scraper/job_get_vehicles.py
+++ scraper/job_get_vehicles.py
@@ -1,14 +1,122 @@
+from dataclasses import dataclass
+import dataclasses
 from time import time
+from datetime import datetime
 import os
 import json
 import asyncio
-
+from typing import Literal, NamedTuple
+from psycopg2.extensions import AsIs
 from aiohttp import ClientSession
 
 import rts_api as rts
 from scraper.log import log
 from scraper.types import RequestDataType
 from scraper.util import chunk
+
+
+class Point(NamedTuple):
+    lat: float
+    lon: float
+
+
+@dataclass(frozen=True)
+class MiscData:
+    """
+    Unused data from the vehicle response
+    _or is a reserved keyword in python, so it is prefixed with an underscore
+    """
+
+    tmstmp: str
+    tatripid: str
+    origtatripno: str
+    tablockid: str
+    zone: str
+    mode: int
+    srvtmstmp: str
+    oid: str
+    _or: bool
+    rid: str
+    blk: int
+    tripdyn: int
+    stst: int
+    stsd: str
+
+
+@dataclass(frozen=True)
+class VehicleReponse:
+    vehicle_id: int
+    coord: Point
+    heading: int
+
+    route_num: int
+    pattern_id: int
+    destination: str
+    pdist: int
+
+    is_delayed: bool
+    speed_mph: int
+
+    passenger_load: Literal["FULL", "HALF_EMPTY", "EMPTY", "N/A"]
+    trip_id: int
+
+    misc_data: MiscData
+
+    def to_sql_tuple(self):
+        column_name_value_map = [
+            ("vehicle_id", self.vehicle_id),
+            ("coord", f"({self.coord.lat},{self.coord.lon})"),
+            ("heading", self.heading),
+            ("route_num", self.route_num),
+            ("pattern_id", self.pattern_id),
+            ("destination", self.destination),
+            ("pdist", self.pdist),
+            ("is_delayed", self.is_delayed),
+            ("speed_mph", self.speed_mph),
+            ("passenger_load", self.passenger_load),
+            ("trip_id", self.trip_id),
+            ("misc_data", json.dumps(dataclasses.asdict(self.misc_data))),
+            (
+                "geom",
+                AsIs(
+                    f"ST_SetSRID(ST_MakePoint({self.coord.lon}, {self.coord.lat}), 4326)"
+                ),
+            ),
+        ]
+
+        return tuple(zip(*column_name_value_map))
+
+
+def deserialize_vehicle_response(response):
+    return VehicleReponse(
+        vehicle_id=int(response["vid"]),
+        coord=Point(lat=float(response["lat"]), lon=float(response["lon"])),
+        heading=int(response["hdg"]),
+        route_num=int(response["rt"]),
+        pattern_id=int(response["pid"]),
+        destination=response["des"],
+        pdist=int(response["pdist"]),
+        is_delayed=bool(response["dly"]),
+        speed_mph=int(response["spd"]),
+        passenger_load=response["psgld"],
+        trip_id=int(response["tripid"]),
+        misc_data=MiscData(
+            tmstmp=response["tmstmp"],
+            tatripid=response["tatripid"],
+            origtatripno=response["origtatripno"],
+            tablockid=response["tablockid"],
+            zone=response["zone"],
+            mode=response["mode"],
+            srvtmstmp=response["srvtmstmp"],
+            oid=response["oid"],
+            _or=response["or"],
+            rid=response["rid"],
+            blk=response["blk"],
+            tripdyn=response["tripdyn"],
+            stst=response["stst"],
+            stsd=response["stsd"],
+        ),
+    )
 
 
 async def job_get_vehicles(session: ClientSession, con, req: RequestDataType):
@@ -52,10 +160,28 @@ async def job_get_vehicles(session: ClientSession, con, req: RequestDataType):
     if len(results):
         try:
             cur = con.cursor()
+            # Insert raw response
             cur.execute(
-                f"insert into {req.db_table_name} values(%s, %s)",
+                f"INSERT INTO {req.db_table_name} VALUES (%s, %s)",
                 (xtime, json.dumps(results)),
             )
+
+            # Insert parsed response
+            for r in map(deserialize_vehicle_response, results):
+                columns, values = r.to_sql_tuple()
+
+                # Insert request time as first column
+                columns = ("request_time", *columns)
+                values = (datetime.fromtimestamp(xtime / 1000), *values)
+
+                str_columns = ",".join(columns)
+                str_insert = ",".join(["%s"] * len(values))
+
+                cur.execute(
+                    f"INSERT INTO data_bus_location ({str_columns}) VALUES ({str_insert})",
+                    values,
+                )
+
             con.commit()
 
             log(
