a2df163b1d06cf9b249a9d549a0543cec8c3b7bb
Noah Bresler <noahbresler2002@gmail.com>
2023-03-06 11:03:22 -0500

    Back fill patterns

---
+++ back_fill/patterns/back_fill_pattern.py
@@ -0,0 +1,98 @@
+from datetime import datetime
+import os
+import json
+from typing import Any, List
+
+from dotenv import load_dotenv
+import psycopg2
+
+from scraper.jobs.job_get_patterns import deserialize_pattern_response
+
+load_dotenv()
+
+con = psycopg2.connect(
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    application_name="Backfill Pattern",
+)
+
+columns = (
+    "request_time",
+    "pattern_id",
+    "direction",
+    "reported_length",
+    "waypoints",
+    "stops",
+    "detour",
+)
+str_columns = ",".join(columns)
+str_insert = ",".join(["%s"] * len(columns))
+str_insert = f"({str_insert})"
+
+
+# chunk_size = 1
+chunk_size = 1
+cur = con.cursor()
+
+# for i in range(1):
+for i in range(1000000):
+    print(f"Processing chunk {i} ({i * chunk_size})")
+    cur.execute(f"SELECT * FROM request_get_patterns OFFSET {i * chunk_size} LIMIT 1")
+    rows = cur.fetchall()
+
+    print(f"Processing {len(rows)} rows...")
+
+    start = datetime.now()
+
+    result_batch: List[Any] = []
+
+    for insert_time, response in rows:
+        data = json.loads(response)
+        for response in data:
+
+            for pattern in map(
+                deserialize_pattern_response, response["bustime-response"]["ptr"]
+            ):
+                # columns, values = pattern.to_sql_tuple()
+                insert_cols, values = pattern.to_sql_dict()
+
+                # Insert request time as first column
+                # values = (datetime.fromtimestamp(insert_time / 1000), *values)
+                values["request_time"] = datetime.fromtimestamp(insert_time / 1000)
+                result_batch.append(values)
+
+                # cur.execute(
+                #     f"INSERT INTO data_patterns({str_columns}) VALUES({insert_cols}) ON CONFLICT DO NOTHING",
+                #     values,
+                # )
+
+    print(f"Inserting {len(result_batch)} rows...")
+
+    if len(result_batch) > 0:
+        insertion_args = ",".join(
+            [
+                cur.mogrify(
+                    "(%(request_time)s, %(pattern_id)s, %(direction)s, %(reported_length)s, %(waypoints)s::jsonb[], %(stops)s::jsonb[], %(detour)s::jsonb)",
+                    v,
+                ).decode("utf-8")
+                for v in result_batch
+            ]
+        )
+        cur.execute(
+            f"INSERT INTO data_patterns ({str_columns}) VALUES {insertion_args} ON CONFLICT DO NOTHING"
+        )
+        con.commit()
+
+    end = datetime.now()
+    print(f"Processed {len(rows)} rows in {end - start}")
+
+    if len(rows) == 0:
+        break
+
+print("done")
+
+
+con.close()
--- scraper/jobs/job_get_patterns.py
+++ scraper/jobs/job_get_patterns.py
@@ -1,13 +1,154 @@
+from dataclasses import dataclass, asdict
 from time import time
 import os
 import json
 import asyncio
+from typing import Any, List, Literal, Optional
+from psycopg2.extras import Json
 
 from aiohttp import ClientSession
 
 import rts_api as rts
 from scraper.log import log
 from scraper.data_types import RequestDataType
+
+
+@dataclass(frozen=True, kw_only=True)
+class Waypoint:
+    seq: int
+    lat: float
+    lon: float
+
+    type: Literal["W"] | Literal["S"] = "W"
+
+
+@dataclass(frozen=True, kw_only=True)
+class Stop(Waypoint):
+    stop_id: str
+    stop_name: str
+    pdist: float
+
+    type: Literal["S"] = "S"
+
+
+@dataclass(frozen=True, kw_only=True)
+class Detour:
+    detour_id: str
+
+    stops: List[Stop]
+    waypoints: List[Waypoint]
+
+
+@dataclass(frozen=True)
+class PatternResponse:
+    pattern_id: int
+    direction: Literal["OUTBOUND", "INBOUND"]
+    reported_length: float
+
+    waypoints: List[Waypoint]
+    stops: List[Stop]
+
+    detour: Optional[Detour]
+
+    def to_sql_tuple(self):
+        column_name_value_map = [
+            ("pattern_id", self.pattern_id),
+            ("direction", self.direction),
+            ("reported_length", self.reported_length),
+            ("waypoints", Json([asdict(w) for w in self.waypoints])),
+            # ("waypoints", Json(list(map(asdict, self.waypoints)))),
+            ("stops", Json([asdict(s) for s in self.stops])),
+            # ("stops", Json(list(map(asdict, self.stops)))),
+            ("detour", Json(asdict(self.detour) if self.detour else None)),
+        ]
+
+        return tuple(zip(*column_name_value_map))
+
+    def to_sql_dict(self):
+        return (
+            "%(request_time)s, %(pattern_id)s, %(direction)s, %(reported_length)s, %(waypoints)s::jsonb[], %(stops)s::jsonb[], %(detour)s::jsonb",
+            {
+                "pattern_id": self.pattern_id,
+                "direction": self.direction,
+                "reported_length": self.reported_length,
+                "waypoints": [Json(asdict(w)) for w in self.waypoints],
+                "stops": [Json(asdict(s)) for s in self.stops],
+                "detour": Json(asdict(self.detour) if self.detour else None),
+            },
+        )
+
+
+def deserialize_pattern_response(response: Any) -> PatternResponse:
+    # Build waypoints
+    waypoints: List[Waypoint] = []
+    stops: List[Stop] = []
+
+    for detour_point in response["pt"]:
+        if detour_point["typ"] == "W":
+            waypoints.append(
+                Waypoint(
+                    seq=int(detour_point["seq"]),
+                    lat=float(detour_point["lat"]),
+                    lon=float(detour_point["lon"]),
+                )
+            )
+        elif detour_point["typ"] == "S":
+            stops.append(
+                Stop(
+                    seq=int(detour_point["seq"]),
+                    lat=float(detour_point["lat"]),
+                    lon=float(detour_point["lon"]),
+                    stop_id=detour_point["stpid"],
+                    stop_name=detour_point["stpnm"],
+                    pdist=float(detour_point["pdist"]),
+                )
+            )
+
+    detour = None
+    if (
+        "dtrid" in response
+        and "dtrpt" in response
+        and response["dtrid"]
+        and response["dtrpt"]
+    ):
+        detour_stops: List[Stop] = []
+        detour_waypoints: List[Waypoint] = []
+
+        for detour_point in response["dtrpt"]:
+            if detour_point["typ"] == "W":
+                detour_waypoints.append(
+                    Waypoint(
+                        seq=int(detour_point["seq"]),
+                        lat=float(detour_point["lat"]),
+                        lon=float(detour_point["lon"]),
+                    )
+                )
+            elif detour_point["typ"] == "S":
+                detour_stops.append(
+                    Stop(
+                        seq=int(detour_point["seq"]),
+                        lat=float(detour_point["lat"]),
+                        lon=float(detour_point["lon"]),
+                        stop_id=detour_point["stpid"],
+                        stop_name=detour_point["stpnm"],
+                        pdist=float(detour_point["pdist"]),
+                    )
+                )
+
+        detour = Detour(
+            detour_id=response["dtrid"],
+            stops=detour_stops,
+            waypoints=detour_waypoints,
+        )
+
+    return PatternResponse(
+        pattern_id=int(response["pid"]),
+        direction=response["rtdir"],
+        reported_length=float(response["ln"]),
+        waypoints=waypoints,
+        stops=stops,
+        detour=detour,
+    )
 
 
 async def job_get_patterns(session: ClientSession, con, req: RequestDataType):
